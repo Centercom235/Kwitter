fa3cea39b2561254aa108bc1d744cc5c817a1842
Centercom235 <79948838+Centercom235@users.noreply.github.com>
2021-09-22 20:03:52 -0600

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -14,14 +14,14 @@ var firebaseConfig = {
     room_name=localStorage.getItem("room_name");
     function send(){
           msg=document.getElementById("msg").value;
-firebase.database().ref(room_name).push({
+          firebase.database().ref(room_name).push({
       name:user_name,
       message:msg,
       like:0
 });
 document.getElementById("msg").value="";
     }
-function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+    function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
          firebase_message_id = childKey;
          message_data = childData;
 //Start code
@@ -33,7 +33,7 @@ like=message_data['like'];
 name_with_tag="<h4>" + username+"<img class='user_tick' src='tick.png'> </h4>";
 message_with_tag="<h4 class='message_h4'>" + message+"</h4>";
 like_button ="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='updateLike(this.id)'>";
- span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: "+ like +"</span></button><hr>";
+span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: "+ like +"</span></button><hr>";
 row=name_with_tag+message_with_tag+like_button+span_with_tag;
 document.getElementById("output").innerHTML+=row;
 
